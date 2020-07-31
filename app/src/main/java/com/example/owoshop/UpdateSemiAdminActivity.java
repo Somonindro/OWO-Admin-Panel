ce7608a01d35afb0c64546781725a50d7fb0c83b
18050 <1805056@ugrad.cse.buet.ac.bd>
2020-07-31 19:56:20 +0600

    Semi Admin Updating corrected.

--- app/src/main/java/com/example/owoshop/UpdateSemiAdminActivity.java
+++ app/src/main/java/com/example/owoshop/UpdateSemiAdminActivity.java
@@ -1,5 +1,6 @@
 package com.example.owoshop;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,11 +9,16 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.model.semi_admins;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.squareup.picasso.Picasso;
 
 import org.w3c.dom.Text;
@@ -23,6 +29,7 @@ public class UpdateSemiAdminActivity extends AppCompatActivity {
     private ImageView admin_profile_pic;
     private Switch approveShop,maintainShop,addProducts,updateProducts,createOffers,maintainUsers,messaging;
     private Button updateAdminBtn;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +40,7 @@ public class UpdateSemiAdminActivity extends AppCompatActivity {
 
         clickedAdminName = (TextView)findViewById(R.id.clicked_admin_name);
         clickedAdminMobile = findViewById(R.id.clicked_admin_mobile);
+        progressBar = findViewById(R.id.clicked_complete_progress);
 
         approveShop = (Switch)findViewById(R.id.clicked_approve_shop);
         maintainShop = (Switch)findViewById(R.id.clicked_maintain_shop);
@@ -45,7 +53,7 @@ public class UpdateSemiAdminActivity extends AppCompatActivity {
         updateAdminBtn = (Button)findViewById(R.id.update_admin_btn);
 
 
-        com.example.model.semi_admins semiAdmins = (semi_admins) getIntent().
+        final com.example.model.semi_admins semiAdmins = (semi_admins) getIntent().
                 getSerializableExtra("Semi Admin");//Getting the required class from the SemiAdminActivity
 
 
@@ -61,14 +69,48 @@ public class UpdateSemiAdminActivity extends AppCompatActivity {
         updateProducts.setChecked(semiAdmins.getUpdate_products());
         createOffers.setChecked(semiAdmins.getCreate_offers());
         maintainUsers.setChecked(semiAdmins.getMaintain_users());
-
+        messaging.setChecked(semiAdmins.getMessaging());
 
         updateAdminBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(UpdateSemiAdminActivity.this, "Semi-admin updated successfully.", Toast.LENGTH_SHORT).show();
-                Intent intent=new Intent(UpdateSemiAdminActivity.this,HomeActivity.class);
-                startActivity(intent);
+
+                DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("Semi Admins");
+
+                //Getting the current state for updating to database
+                semiAdmins.setApprove_shop(approveShop.isChecked());
+                semiAdmins.setMaintain_shops(maintainShop.isChecked());
+                semiAdmins.setAdd_products(addProducts.isChecked());
+                semiAdmins.setUpdate_products(updateProducts.isChecked());
+                semiAdmins.setCreate_offers(createOffers.isChecked());
+                semiAdmins.setMaintain_users(maintainUsers.isChecked());
+                semiAdmins.setMessaging(messaging.isChecked());
+
+
+                progressBar.setVisibility(View.VISIBLE);
+
+                reference.child(semiAdmins.getPhone()).setValue(semiAdmins).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+
+                        if(task.isSuccessful())
+                        {
+                            Toast.makeText(UpdateSemiAdminActivity.this, "Semi-admin info updated successfully.", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(UpdateSemiAdminActivity.this,HomeActivity.class);
+                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                            startActivity(intent);
+                            finish();
+                        }
+
+                        else
+                        {
+                            Toast.makeText(UpdateSemiAdminActivity.this, "Can not update Semi admin's data"+task.getException().toString(), Toast.LENGTH_SHORT).show();
+                            progressBar.setVisibility(View.GONE);
+                        }
+                    }
+                });
+
+
             }
         });
 
