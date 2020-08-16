9d99b7cacb25cafe240b941fdc4f8d6fafc2e5c4
18050 <1805056@ugrad.cse.buet.ac.bd>
2020-08-16 22:46:23 +0600

    Swipe refresh added.

--- app/src/main/java/com/OwoDokan/owoshop/ProductAvailabilityActivity.java
+++ app/src/main/java/com/OwoDokan/owoshop/ProductAvailabilityActivity.java
@@ -5,8 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.paging.PagedList;
+import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.os.Bundle;
 
@@ -18,29 +20,52 @@ public class ProductAvailabilityActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
     RecyclerView.LayoutManager layoutManager;
+    private SwipeRefreshLayout swipeRefreshLayout;
+    private ItemAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_availability);
 
+
+        getProducts();
+
+        swipeRefreshLayout = findViewById(R.id.swipe_refresh);
+        swipeRefreshLayout.setColorSchemeResources(R.color.colorPrimary);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                getProducts();
+            }
+        });
+
         recyclerView = findViewById(R.id.product_availability_recyclerview_id);
 
-        recyclerView.setHasFixedSize(true);
-        layoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(layoutManager);
+    }
 
+
+    public void getProducts() {
         ItemViewModel itemViewModel = ViewModelProviders.of(this).get(ItemViewModel.class);
-        final ItemAdapter adapter = new ItemAdapter(this);
+        adapter = new ItemAdapter(this);
 
         itemViewModel.itemPagedList.observe(this, new Observer<PagedList<Products>>() {
             @Override
             public void onChanged(@Nullable PagedList<Products> items) {
                 adapter.submitList(items);
+                showOnRecyclerView();
             }
         });
+    }
 
+
+    private void showOnRecyclerView() {
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
         recyclerView.setAdapter(adapter);
+        swipeRefreshLayout.setRefreshing(false);
+        adapter.notifyDataSetChanged();
     }
 
 }
